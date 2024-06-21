3166e77e8e20329fa4e63f8d1a4ecb4545246582
Maxi <maxi.inigo@gmail.com>
2024-06-21 11:53:56 -0300

    chat para convertir texto en audio

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -4,5 +4,6 @@ export * from './chat-input-boxes/TextMessageBox';
 export * from './chat-input-boxes/TextMessageBoxFile';
 export * from './chat-input-boxes/TextMessageBoxSelect';
 export * from './chat-bubbles/GptMessage';
+export * from './chat-bubbles/GptMessageAudio';
 export * from './chat-bubbles/GptOrthographyMessage';
 export * from './loaders/TypingLoader';
--- src/presentation/pages/text-to-audio/TextToAudioPage.tsx
+++ src/presentation/pages/text-to-audio/TextToAudioPage.tsx
@@ -1,5 +1,96 @@
-export const TextToAudioPage = () => {
-  return (
-    <div>TextToAudioPage</div>
-  )
+import { useState } from "react";
+import {
+  GptMessage,
+  GptMessageAudio,
+  MyMessage,
+  TextMessageBoxSelect,
+  TypingLoader,
+} from "../../components";
+import { textToAudioUseCase } from "../../../core/use-cases";
+
+interface TextMessage {
+  text: string;
+  isGpt: boolean;
+  type: 'text';
 }
+
+interface AudioMessage {
+  text: string;
+  isGpt: boolean;
+  audio: string;
+  type: 'audio';
+}
+
+type Message = TextMessage | AudioMessage;
+
+const voices = [
+  { id: "nova", text: "Nova" },
+  { id: "alloy", text: "Alloy" },
+  { id: "echo", text: "Echo" },
+  { id: "fable", text: "Fable" },
+  { id: "onyx", text: "Onyx" },
+  { id: "shimmer", text: "Shimmer" },
+]
+
+export const TextToAudioPage = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const handlePost = async (text: string, selectedVoice: string) => {
+    setIsLoading(true);
+    setMessages((prev) => [...prev, { text: text, isGpt: false, type: 'text'}]);
+    const { ok, message, audioUrl } = await textToAudioUseCase(text, selectedVoice);
+    setIsLoading(false);
+    if (!ok) {
+      setMessages((prev) => [
+        ...prev,
+        {
+          text: "No se pudo realizar el audio",
+          isGpt: true,
+          type: 'text'
+        },
+      ]);
+    } else {
+      setMessages((prev) => [
+        ...prev,
+        {
+          text: `${selectedVoice} - ${message}`,
+          isGpt: true,
+          audio: audioUrl!,
+          type: 'audio',
+        },
+      ]);
+    }
+  };
+
+  return (
+    <div className="chat-container">
+      <div className="chat-messages">
+        <div className="grid grid-cols-12 gap-y-2">
+          <GptMessage text="¿Qué desea escuchar?" />
+          {messages.map((message, index) =>
+            message.isGpt ? (
+              message.type === 'audio' ? (
+                <GptMessageAudio key={index} text={message.text} audio={message.audio} />
+              ) : (
+                <GptMessage key={index} text={message.text} />
+              )
+            ) : (
+              <MyMessage key={index} text={message.text} />
+            )
+          )}
+          {isLoading && (
+            <div className="col-start-1 col-end-12 fade-in">
+              <TypingLoader className="fade-in" />
+            </div>
+          )}
+        </div>
+      </div>
+      <TextMessageBoxSelect
+        onSendMessage={handlePost}
+        placeholder="Escribe aquí lo que deseas..."
+        options={voices}
+      />
+    </div>
+  );
+};
