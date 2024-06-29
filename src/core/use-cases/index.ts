a3449b515cd3fc9435ab4a4694b7cf62ba78a9e1
Maxi <maxi.inigo@gmail.com>
2024-06-29 11:17:57 -0300

    generación de imagen

--- src/core/use-cases/index.ts
+++ src/core/use-cases/index.ts
@@ -1,4 +1,5 @@
 export * from "./audio-to-text.use-case";
+export * from "./image-generation.use-case";
 export * from "./orthography.use-case";
 export * from "./pros-cons.use-case";
 export * from "./pros-cons-stream.use-case";
--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -5,5 +5,6 @@ export * from './chat-input-boxes/TextMessageBoxFile';
 export * from './chat-input-boxes/TextMessageBoxSelect';
 export * from './chat-bubbles/GptMessage';
 export * from './chat-bubbles/GptMessageAudio';
+export * from './chat-bubbles/GptMessageImage';
 export * from './chat-bubbles/GptOrthographyMessage';
 export * from './loaders/TypingLoader';
